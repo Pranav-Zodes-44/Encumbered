7dfd5d2401483048f1bb7abee300286dae6489df
Pranav Reddy <pranav@quicket.co.za>
2023-11-27 22:31:19 +0200

    Successfully able to add mula to party gold

--- main.py
+++ main.py
@@ -14,104 +14,127 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or("!!"), intents=INTE
 
 @bot.event
 async def on_ready():
-    await bot.tree.sync()
-    print("Bot connected.")
+	await bot.tree.sync()
+	print("Bot connected.")
 
 
 @bot.tree.command(
-    name="gold",
-    description="Tracks the party gold"
+	name="gold",
+	description="Tracks the party gold"
 )
 async def slash_party_gold(
-    interaction: discord.Interaction
+	interaction: discord.Interaction
 ):
-    user = await user_service.get_or_insert_user(interaction.user.id)
+	user = await user_service.get_or_insert_user(interaction.user.id)
 
-    has_party = await user_service.check_user_party(user, interaction)
+	has_party = await user_service.check_user_party(user, interaction)
 
-    if (has_party):
-        gold = await party_service.get_party_gold(user.currentParty)
-        #use a new service to map gold to PartyGoldDto and send as embed message
-        await interaction.response.send_message(f"Party Gold = {gold.value}")
+	if (has_party):
+		gold = await party_service.get_party_gold(user.currentParty)
+		
+		embed = embed_service.get_gold_embed(dto.PartyGoldDto(**gold.value), user, interaction)
+		await interaction.response.send_message(embed=embed)
 
 
 @bot.tree.command(
-    name="addmula",
-    description="Add money to party"
+	name="addmula",
+	description="Add money to party"
 )
 async def slash_add_party_gold(
-    interaction: discord.Interaction,
-    gold: int
+	interaction: discord.Interaction,
+	pp: int = 0,
+	gp: int = 0,
+	sp: int = 0,
+	cp: int = 0
 ):
-    user = await user_service.get_or_insert_user(interaction.user.id)
+	user = await user_service.get_or_insert_user(interaction.user.id)
 
-    has_party = await user_service.check_user_party(user, interaction)
+	has_party = await user_service.check_user_party(user, interaction)
 
-    if (has_party):
-        await interaction.response.send_message(f"Adding gold: {gold}")
+	if (has_party):
+		gold_item = await party_service.get_party_gold(user.currentParty)
+		current_gold = dto.PartyGoldDto(**gold_item.value)
 
+		gold_change = dto.PartyGoldDto(
+			pp=pp,
+			gp=gp,
+			sp=sp,
+			cp=cp
+		)
+		new_gold = party_service.add_mula(current_gold, gold_change)
+		
+		embed = embed_service.get_gold_embed(current_gold, user, interaction, new_gold)
+		message = await interaction.channel.send(embed=embed)
+		await party_money_change(message, user, interaction, new_gold)
 
 @bot.tree.command(
-    name="setparty",
-    description="Set your current party to an existing or new party"
+	name="setparty",
+	description="Set your current party to an existing or new party"
 )
 async def slash_set_party(
-    interaction: discord.Interaction,
-    name: str
+	interaction: discord.Interaction,
+	name: str
 ):
-    discord_user = interaction.user
+	discord_user = interaction.user
 
-    await user_service.set_party(discord_user.id, name)
+	await user_service.set_party(discord_user.id, name)
 
-    await interaction.response.send_message(f"Set party to `{name}` for {discord_user.mention}")
+	await interaction.response.send_message(f"Set party to `{name}` for {discord_user.mention}")
 
 
 @bot.tree.command(
-    name="setgold",
-    description="Set the gold of your current party"
+	name="setgold",
+	description="Set the gold of your current party"
 )
 async def slash_set_gold(
-    interaction: discord.Interaction,
-    pp: int = 0,
-    gp: int = 0,
-    sp: int = 0,
-    cp: int = 0
+	interaction: discord.Interaction,
+	pp: int = 0,
+	gp: int = 0,
+	sp: int = 0,
+	cp: int = 0
 ):
-    user = await user_service.get_or_insert_user(interaction.user.id)
+	user = await user_service.get_or_insert_user(interaction.user.id)
 
-    has_party = await user_service.check_user_party(user, interaction)
+	has_party = await user_service.check_user_party(user, interaction)
 
-    if (has_party):
-        new_party_gold = dto.PartyGoldDto(
-            pp=pp,
-            gp=gp,
-            sp=sp,
-            cp=cp
-        )
+	if (has_party):
+		new_party_gold = dto.PartyGoldDto(
+			pp=pp,
+			gp=gp,
+			sp=sp,
+			cp=cp
+		)
 
-        await interaction.response.send_message(f'Getting gold for {user.currentParty} :timer:')
+		await interaction.response.send_message(f'Getting gold for {user.currentParty} :timer:')
 
-        current_party_gold = await party_service.get_party_gold(user.currentParty)
+		current_party_gold = await party_service.get_party_gold(user.currentParty)
 
-        embed = embed_service.get_gold_embed(dto.PartyGoldDto(**current_party_gold.value), user, interaction, new_party_gold)
+		embed = embed_service.get_gold_embed(dto.PartyGoldDto(**current_party_gold.value), user, interaction, new_party_gold)
 
-        message = await interaction.channel.send(embed=embed)
-        await message.add_reaction('🫡')
+		message = await interaction.channel.send(embed=embed)
+		await party_money_change(message, user, interaction, new_party_gold)
 
-        try:
-            await bot.wait_for('reaction_add',
-                            check=lambda reaction, user: reaction.emoji == '🫡' and user.id == interaction.user.id, timeout=15)
-        except asyncio.TimeoutError as e:
-            await message.delete()
-            await interaction.channel.send("Timer expired, please try again.")
-        
-        await party_service.set_party_gold(user.currentParty, new_party_gold)
 
-        embed = embed_service.get_gold_embed(new_party_gold, user, interaction)
-        await message.clear_reactions()
-        await message.edit(content=f"**Successfully set party gold for {user.currentParty} :money_mouth:**", embed=embed)
-        
+async def party_money_change(
+		message: discord.Message, 
+		user: dto.UserDto, 
+		interaction: discord.Interaction, 
+		new_party_gold: dto.PartyGoldDto
+):
+	await message.add_reaction('🫡')
 
+	try:
+		await bot.wait_for('reaction_add',
+						check=lambda reaction, user: reaction.emoji == '🫡' and user.id == interaction.user.id, timeout=15)
+	except asyncio.TimeoutError as e:
+		await message.delete()
+		await interaction.channel.send("Timer expired, please try again.")
+	
+	await party_service.set_party_gold(user.currentParty, new_party_gold)
+
+	embed = embed_service.get_gold_embed(new_party_gold, user, interaction)
+	await message.clear_reactions()
+	await message.edit(content=f"**Successfully updated party gold for {user.currentParty} :money_mouth:**", embed=embed)
 
 
 bot.run(TOKEN)
--- services/party_service.py
+++ services/party_service.py
@@ -1,15 +1,26 @@
 from services import mongo, dto
 
 async def get_party_gold(party_name: str):
-    gold = await mongo.find_one({"name": "Gold"}, party_name)
+	gold = await mongo.find_one({"name": "Gold"}, party_name)
 
-    if (gold == None):
-        await mongo.insert_one(dto.ItemDto(name="Gold", rarity="Gold"), party_name)
-        gold = await mongo.find_one({"name": "Gold"}, party_name)
+	if (gold == None):
+		await mongo.insert_one(dto.ItemDto(name="Gold", rarity="Gold"), party_name)
+		gold = await mongo.find_one({"name": "Gold"}, party_name)
 
-    del gold["_id"]
-    
-    return dto.ItemDto(**gold)
+	del gold["_id"]
+	
+	return dto.ItemDto(**gold)
+
 
 async def set_party_gold(party_name: str, new_party_gold: dto.PartyGoldDto):
-    await mongo.update_one({"name": "Gold"}, {"value": new_party_gold.__dict__}, party_name)
+	await mongo.update_one({"name": "Gold"}, {"value": new_party_gold.__dict__}, party_name)
+
+
+def add_mula(current_party_gold: dto.PartyGoldDto, gold_change: dto.PartyGoldDto):
+	current_party_gold.pp += gold_change.pp
+	current_party_gold.gp += gold_change.gp
+	current_party_gold.sp += gold_change.sp
+	current_party_gold.cp += gold_change.cp
+	
+	return current_party_gold
+
