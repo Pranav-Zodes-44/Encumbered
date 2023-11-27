37a354ecef1e8cc09f8c0cec5b0d56522779bf55
PranavR <ggpranav44@gmail.com>
2023-11-27 13:39:47 +0200

    mongo working with general mongo methods to be used everywhere

--- services/mongo.py
+++ services/mongo.py
@@ -1,3 +1,21 @@
-import asyncio
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
+import pprint
+from dotenv import load_dotenv
+from services import dto
+
+load_dotenv()
+_mongostring = os.getenv('MONGO')
+_client = AsyncIOMotorClient(_mongostring, server_api=ServerApi('1'))
+db = _client.main
+
+async def insert_one(document, collection: str):
+    await db[collection].insert_one(document.__dict__)
+
+async def update_one(filter: dict, new_value: dict, collection: str):
+    await db[collection].update_one(filter, {"$set": new_value})
+
+async def find_one(filter: dict, collection: str):
+    document = await db[collection].find_one(filter)
+    return document
